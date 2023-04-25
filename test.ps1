677c6ea0915622cd117692c7ed94e93d0055676a
salihsert <salihsert18@gmail.com>
2023-04-25 18:19:21 +0200

    refactor

--- test.ps1
+++ test.ps1
@@ -1,3 +1,10 @@
+param (
+    [Parameter(Mandatory)]
+    [string]$ResourceGroupName,
+    [Parameter(Mandatory)]
+    [string]$ApplicationGatewayName
+)
+
 $variable1 = New-AzApplicationGatewayFirewallMatchVariable -VariableName RequestUri
 $condition1 = New-AzApplicationGatewayFirewallCondition -MatchVariable $variable1 -Operator Contains -MatchValue @("test1", "test2") -NegationCondition $false
 $customRule1 = New-AzApplicationGatewayFirewallCustomRule -Name "Rule1Allow" -Priority 20 -RuleType MatchRule-MatchConditions $condition1  -Action Allow 
@@ -6,13 +13,11 @@ $variable2 = New-AzApplicationGatewayFirewallMatchVariable -VariableName Request
 $condition2 = New-AzApplicationGatewayFirewallCondition -MatchVariable $variable2 -Operator Contains -MatchValue "bots" -Transform Lowercase -NegationCondition $false
 $customRule2 = New-AzApplicationGatewayFirewallCustomRule -Name "blockBots" -Priority 10 -RuleType MatchRule -MatchConditions $condition2 -Action Block
 
-$appGateway = Get-AzApplicationGateway -Name "myAppGateway" -ResourceGroupName "myResourceGroup"
 $policySettingGlobal = New-AzApplicationGatewayFirewallPolicySetting -State Enabled -Mode Prevention
-$customWafPolicy = New-AzApplicationGatewayFirewallPolicy -Name "myCustomWafPolicy-myResourceGroup" -ResourceGroupName "myResourceGroup" -Location "westeurope" -PolicySetting $policySettingGlobal -CustomRule $customRule1, $customRule2 -Force
+$customWafPolicy = New-AzApplicationGatewayFirewallPolicy -Name "myCustomWafPolicy-$ResourceGroupName" -ResourceGroupName $ResourceGroupName -Location "westeurope" -PolicySetting $policySettingGlobal -CustomRule $customRule1, $customRule2 -Force
 
+$appGateway = Get-AzApplicationGateway -Name $ApplicationGatewayName -ResourceGroupName $ResourceGroupName
 Set-AzApplicationGatewayFirewallPolicy -InputObject $customWafPolicy
 $appGateway.FirewallPolicy = $customWafPolicy
 $appGateway.ForceFirewallPolicyAssociation = $true
 Set-AzApplicationGateway -ApplicationGateway $appGateway
-
-
